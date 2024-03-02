60a6717e9824a834a0bb049625a29cf7c718b616
klakd <arkadiusz.klak@gmail.com>
2024-03-02 16:01:35 +0100

    Initial commit

---
+++ src/main/java/com/example/openfeignforquotersextended/OpenFeignForQuotersExtendedApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfeignforquotersextended;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignForQuotersExtendedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignForQuotersExtendedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openfeignforquotersextended/OpenFeignForQuotersExtendedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openfeignforquotersextended;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignForQuotersExtendedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
