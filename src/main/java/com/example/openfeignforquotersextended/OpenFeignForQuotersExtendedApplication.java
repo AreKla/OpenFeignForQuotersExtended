acd0d03d07eada2309d8378d3b80520a1c3a82ad
klakd <arkadiusz.klak@gmail.com>
2024-03-02 18:33:48 +0100

    Refactored OpenFeignForQuotersExtendedApplication

--- src/main/java/com/example/openfeignforquotersextended/OpenFeignForQuotersExtendedApplication.java
+++ src/main/java/com/example/openfeignforquotersextended/OpenFeignForQuotersExtendedApplication.java
@@ -2,12 +2,25 @@ package com.example.openfeignforquotersextended;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class OpenFeignForQuotersExtendedApplication {
+
+    QuoterExtendProxy quoterExtendClient;
+
+    public OpenFeignForQuotersExtendedApplication(QuoterExtendProxy quoterExtendClient) {
+        this.quoterExtendClient = quoterExtendClient;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OpenFeignForQuotersExtendedApplication.class, args);
     }
 
+    @EventListener(ApplicationStartedEvent.class)
+    public void makeRequestToQuoterExtend() {
+        String response = quoterExtendClient.request();
+        System.out.println(response);
+    }
 }
