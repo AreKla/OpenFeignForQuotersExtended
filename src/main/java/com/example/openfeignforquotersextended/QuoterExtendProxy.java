493b1b51c9d26c8c2c8a0c6552b89d76c6d437db
klakd <arkadiusz.klak@gmail.com>
2024-03-07 14:45:40 +0100

    Add @Component annotation to the QuoterExtendProxy class and make minor edits in OpenFeignForQuotersExtendedApplication

--- src/main/java/com/example/openfeignforquotersextended/QuoterExtendProxy.java
+++ src/main/java/com/example/openfeignforquotersextended/QuoterExtendProxy.java
@@ -2,9 +2,11 @@ package com.example.openfeignforquotersextended;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "quoter-extend-server-client")
+@Component
 interface QuoterExtendProxy {
 
     @GetMapping("/api")
